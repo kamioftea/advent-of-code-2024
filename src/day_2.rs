4d45f0812559103291f4c81195d928f638bbfe3a
Jeff Horton <jeff@goblinoid.co.uk>
2024-12-02 17:16:36 +0000

    part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -14,6 +14,10 @@ pub fn run() {
     let reports = parse_input(&contents);
 
     println!("There are {} safe reports", analyse_reports(&reports));
+    println!(
+        "There are {} safe reports with the dampener",
+        analyse_reports_with_dampener(&reports)
+    );
 }
 
 type Report = Vec<u32>;
@@ -44,10 +48,35 @@ fn first_bad_level_pair(report: &Report) -> Option<usize> {
     None
 }
 
+fn without_index(report: &Report, idx: usize) -> Report {
+    let mut new = report.clone();
+    new.remove(idx);
+
+    new
+}
+
+fn report_check_with_dampener(report: &Report) -> bool {
+    if let Some(pair_idx) = first_bad_level_pair(report) {
+        let lower_bound = pair_idx.checked_sub(1).unwrap_or(0);
+        (lower_bound..=(pair_idx + 1))
+            .into_iter()
+            .any(|level_idx| first_bad_level_pair(&without_index(report, level_idx)).is_none())
+    } else {
+        true
+    }
+}
+
 fn analyse_reports(reports: &Vec<Report>) -> usize {
     reports
         .into_iter()
         .filter(|&report| first_bad_level_pair(report).is_none())
+        .count()
+}
+
+fn analyse_reports_with_dampener(reports: &Vec<Report>) -> usize {
+    reports
+        .into_iter()
+        .filter(|&report| report_check_with_dampener(report))
         .count()
 }
 
@@ -65,19 +94,20 @@ mod tests {
             .to_string()
     }
 
+    fn sample_reports() -> Vec<Report> {
+        vec![
+            vec![7, 6, 4, 2, 1],
+            vec![1, 2, 7, 8, 9],
+            vec![9, 7, 6, 2, 1],
+            vec![1, 3, 2, 4, 5],
+            vec![8, 6, 4, 4, 1],
+            vec![1, 3, 6, 7, 9],
+        ]
+    }
+
     #[test]
     fn can_parse_input() {
-        assert_eq!(
-            parse_input(&sample_input()),
-            vec![
-                vec![7, 6, 4, 2, 1],
-                vec![1, 2, 7, 8, 9],
-                vec![9, 7, 6, 2, 1],
-                vec![1, 3, 2, 4, 5],
-                vec![8, 6, 4, 4, 1],
-                vec![1, 3, 6, 7, 9],
-            ]
-        )
+        assert_eq!(parse_input(&sample_input()), sample_reports())
     }
 
     #[test]
@@ -88,20 +118,27 @@ mod tests {
         assert_eq!(first_bad_level_pair(&vec![1, 3, 2, 4, 5]), Some(1));
         assert_eq!(first_bad_level_pair(&vec![8, 6, 4, 4, 1]), Some(2));
         assert_eq!(first_bad_level_pair(&vec![1, 3, 6, 7, 9]), None);
+        assert_eq!(first_bad_level_pair(&vec![4, 3, 6, 7, 9]), Some(1));
+    }
+
+    #[test]
+    fn can_check_if_a_report_is_safe_with_dampener() {
+        assert_eq!(report_check_with_dampener(&vec![7, 6, 4, 2, 1]), true);
+        assert_eq!(report_check_with_dampener(&vec![1, 2, 7, 8, 9]), false);
+        assert_eq!(report_check_with_dampener(&vec![9, 7, 6, 2, 1]), false);
+        assert_eq!(report_check_with_dampener(&vec![1, 3, 2, 4, 5]), true);
+        assert_eq!(report_check_with_dampener(&vec![8, 6, 4, 4, 1]), true);
+        assert_eq!(report_check_with_dampener(&vec![1, 3, 6, 7, 9]), true);
+        assert_eq!(report_check_with_dampener(&vec![5, 3, 4, 7, 9]), true);
     }
 
     #[test]
     fn can_analyse_reports() {
-        assert_eq!(
-            analyse_reports(&vec![
-                vec![7, 6, 4, 2, 1],
-                vec![1, 2, 7, 8, 9],
-                vec![9, 7, 6, 2, 1],
-                vec![1, 3, 2, 4, 5],
-                vec![8, 6, 4, 4, 1],
-                vec![1, 3, 6, 7, 9],
-            ]),
-            2
-        )
+        assert_eq!(analyse_reports(&sample_reports()), 2)
+    }
+
+    #[test]
+    fn can_analyse_reports_with_dampener() {
+        assert_eq!(analyse_reports_with_dampener(&sample_reports()), 4)
     }
 }
