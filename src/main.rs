df5409a8d45a24fba4b3442f539b457cefe708bc
Jeff Horton <jeff@goblinoid.co.uk>
2024-12-03 08:09:24 +0000

    bootstrap day 3

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate text_io;
 mod bootstrap_day;
 mod day_1;
 mod day_2;
+mod day_3;
 mod helpers;
 
 use bootstrap_day::bootstrap_day;
@@ -16,7 +17,11 @@ fn main() {
     io::stdout().flush().unwrap();
 
     let day: u8 = read!();
-    let days: Vec<Box<dyn Fn() -> ()>> = vec![Box::new(|| day_1::run()), Box::new(|| day_2::run())];
+    let days: Vec<Box<dyn Fn() -> ()>> = vec![
+        Box::new(|| day_1::run()),
+        Box::new(|| day_2::run()),
+        Box::new(|| day_3::run()),
+    ];
 
     let start = Instant::now();
     match days.get((day - 1) as usize) {
