7117c6ccb107aaa06ad9986f6357c40b86a50e67
Jeff Horton <jeff@goblinoid.co.uk>
2024-12-04 08:56:14 +0000

    Parse inout to grid

--- src/day_4.rs
+++ src/day_4.rs
@@ -3,6 +3,7 @@
 //!
 
 use std::fs;
+use std::str::FromStr;
 
 /// The entry point for running the solutions with the 'real' puzzle input.
 ///
@@ -12,7 +13,45 @@ pub fn run() {
     let _contents = fs::read_to_string("res/day-4-input.txt").expect("Failed to read file");
 }
 
+#[derive(Eq, PartialEq, Debug)]
+struct Wordsearch {
+    cells: Vec<Vec<char>>,
+}
+
+type CellCoords = (usize, usize);
+
+impl Wordsearch {}
+
+impl FromStr for Wordsearch {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let cells = s.lines().map(|l| l.chars().collect()).collect();
+
+        Ok(Wordsearch { cells })
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::day_4::*;
+    
+    #[test]
+    fn can_parse_input() {
+        let input = "..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....";
 
+        let cells = vec![
+            vec!['.', '.', 'X', '.', '.', '.'],
+            vec!['.', 'S', 'A', 'M', 'X', '.'],
+            vec!['.', 'A', '.', '.', 'A', '.'],
+            vec!['X', 'M', 'A', 'S', '.', 'S'],
+            vec!['.', 'X', '.', '.', '.', '.'],
+        ];
+
+        assert_eq!(Wordsearch::from_str(input), Ok(Wordsearch { cells }));
+    }
 }
