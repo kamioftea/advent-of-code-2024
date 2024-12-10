e8cd60a2ebf0564dfffc7b73330b0cd6e3a3924e
Jeff Horton <jeff@goblinoid.co.uk>
2024-12-10 05:39:36 +0000

    Boostrap day 10

---
+++ src/day_10.rs
@@ -0,0 +1,18 @@
+//! This is my solution for [Advent of Code - Day 10: _Hoof It_](https://adventofcode.com/2024/day/10)
+//!
+//!
+
+use std::fs;
+
+/// The entry point for running the solutions with the 'real' puzzle input.
+///
+/// - The puzzle input is expected to be at `<project_root>/res/day-10-input`
+/// - It is expected this will be called by [`super::main()`] when the user elects to run day 10.
+pub fn run() {
+    let _contents = fs::read_to_string("res/day-10-input.txt").expect("Failed to read file");
+}
+
+#[cfg(test)]
+mod tests {
+
+}
