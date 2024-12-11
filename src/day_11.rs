f8b128453982d679a064b2ee63a21c6f135dcb95
Jeff Horton <jeff@goblinoid.co.uk>
2024-12-11 12:31:56 +0000

    Day 11 - part 1

--- src/day_11.rs
+++ src/day_11.rs
@@ -9,10 +9,99 @@ use std::fs;
 /// - The puzzle input is expected to be at `<project_root>/res/day-11-input`
 /// - It is expected this will be called by [`super::main()`] when the user elects to run day 11.
 pub fn run() {
-    let _contents = fs::read_to_string("res/day-11-input.txt").expect("Failed to read file");
+    let contents = fs::read_to_string("res/day-11-input.txt").expect("Failed to read file");
+    let stones = parse_input(&contents);
+
+    println!(
+        "After 25 blinks there are {} stones",
+        count_after_blinks(&stones, 25)
+    )
+}
+
+fn parse_input(input: &String) -> Vec<u64> {
+    input
+        .trim()
+        .split(" ")
+        .map(|num| num.parse().unwrap())
+        .collect()
+}
+
+fn blink(stones: &Vec<u64>) -> Vec<u64> {
+    stones
+        .into_iter()
+        .flat_map(|&stone| {
+            if stone == 0 {
+                return vec![1];
+            }
+
+            let as_str = format!("{stone}");
+            if as_str.len() % 2 == 0 {
+                let (left, right) = as_str.split_at(as_str.len() / 2);
+                return vec![left.parse().unwrap(), right.parse().unwrap()];
+            }
+
+            return vec![stone * 2024];
+        })
+        .collect()
+}
+
+fn count_after_blinks(stones: &Vec<u64>, number_of_blinks: u64) -> usize {
+    let mut stones = stones.clone();
+
+    for _ in 0..number_of_blinks {
+        stones = blink(&stones)
+    }
+
+    stones.len()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::day_11::*;
+    
+    #[test]
+    fn can_parse_input() {
+        assert_eq!(
+            parse_input(&"0 1 10 99 999\n".to_string()),
+            vec![0, 1, 10, 99, 999]
+        )
+    }
 
+    #[test]
+    fn can_step_stones() {
+        assert_eq!(
+            blink(&vec![0, 1, 10, 99, 999]),
+            vec![1, 2024, 1, 0, 9, 9, 2021976]
+        );
+        assert_eq!(blink(&vec![125, 17]), vec![253000, 1, 7]);
+        assert_eq!(blink(&vec![253000, 1, 7]), vec![253, 0, 2024, 14168]);
+        assert_eq!(
+            blink(&vec![253, 0, 2024, 14168]),
+            vec![512072, 1, 20, 24, 28676032]
+        );
+        assert_eq!(
+            blink(&vec![512072, 1, 20, 24, 28676032]),
+            vec![512, 72, 2024, 2, 0, 2, 4, 2867, 6032]
+        );
+        assert_eq!(
+            blink(&vec![512, 72, 2024, 2, 0, 2, 4, 2867, 6032]),
+            vec![1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32]
+        );
+        assert_eq!(
+            blink(&vec![
+                1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32
+            ]),
+            vec![
+                2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6,
+                0, 3, 2
+            ]
+        );
+    }
+
+    #[test]
+    fn can_count_stones_after_n_blinks() {
+        assert_eq!(count_after_blinks(&vec![125, 17], 6), 22);
+
+        assert_eq!(count_after_blinks(&vec![125, 17], 25), 55312);
+    }
 }
