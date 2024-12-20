fe6a0cf3f8b67dd38fcfc0cb28bd5ae789f9e4ae
Jeff Horton <jeff@goblinoid.co.uk>
2024-12-20 11:23:09 +0000

    Day 20 part 1

--- src/day_20.rs
+++ src/day_20.rs
@@ -2,7 +2,7 @@
 //!
 //!
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 
 /// The entry point for running the solutions with the 'real' puzzle input.
@@ -10,16 +10,90 @@ use std::fs;
 /// - The puzzle input is expected to be at `<project_root>/res/day-20-input`
 /// - It is expected this will be called by [`super::main()`] when the user elects to run day 20.
 pub fn run() {
-    let _contents = fs::read_to_string("res/day-20-input.txt").expect("Failed to read file");
+    let contents = fs::read_to_string("res/day-20-input.txt").expect("Failed to read file");
+    let track = parse_input(&contents);
+
+    println!(
+        "There are {} cheats of at least 100 picoseconds",
+        track.count_cheats_from(100)
+    );
 }
 
 type Coordinates = (usize, usize);
+
+trait CoordinateExtensions: Sized {
+    fn apply(&self, delta: &(isize, isize)) -> Option<Self>;
+}
+
+impl CoordinateExtensions for Coordinates {
+    fn apply(&self, delta: &(isize, isize)) -> Option<Self> {
+        let (r, c) = self;
+        let (dr, dc) = delta;
+
+        let r1 = r.checked_add_signed(*dr);
+        let c1 = c.checked_add_signed(*dc);
+
+        r1.zip(c1)
+    }
+}
 
 #[derive(Eq, PartialEq, Debug)]
 struct RaceTrack {
     course: HashSet<Coordinates>,
     start: Coordinates,
     end: Coordinates,
+}
+
+impl RaceTrack {
+    fn cheats(&self) -> HashMap<(Coordinates, Coordinates), usize> {
+        let mut visited = HashMap::new();
+        let mut cheats = HashMap::new();
+        let mut position = self.start;
+
+        for index in 0.. {
+            visited.insert(position, index);
+            let mut next = position;
+            for delta in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+                let maybe_adjacent = position.apply(&delta);
+                let maybe_next = maybe_adjacent.filter(|coords| self.course.contains(coords));
+
+                if maybe_adjacent.is_some() && maybe_next.is_none() {
+                    let adjacent = maybe_adjacent.unwrap();
+                    let maybe_cheat = adjacent
+                        .apply(&delta)
+                        .and_then(|coords| Some(coords).zip(visited.get(&coords)));
+                    if let Some((start_pos, &start_index)) = maybe_cheat {
+                        cheats.insert((start_pos, position), index - start_index - 2);
+                    }
+                }
+
+                let without_visited = maybe_next.filter(|coords| !visited.contains_key(coords));
+                if without_visited.is_some() {
+                    next = without_visited.unwrap();
+                }
+            }
+
+            if position == self.end {
+                break;
+            }
+
+            if next == position {
+                unreachable!("{position:?} failed to find next position")
+            }
+
+            position = next;
+        }
+
+        cheats
+    }
+
+    fn count_cheats_from(&self, threshold: usize) -> usize {
+        self.cheats()
+            .iter()
+            .map(|(_, saving)| saving)
+            .filter(|&&saving| saving >= threshold)
+            .count()
+    }
 }
 
 fn parse_input(input: &String) -> RaceTrack {
@@ -99,5 +173,25 @@ mod tests {
         .to_string();
 
         assert_eq!(parse_input(&input), example_track());
+    }
+
+    #[test]
+    fn can_list_cheats() {
+        let cheats = example_track().cheats();
+
+        assert_eq!(cheats.get(&((1, 7), (1, 9))), Some(&12));
+        assert_eq!(cheats.get(&((7, 9), (7, 11))), Some(&20));
+        assert_eq!(cheats.get(&((7, 8), (9, 8))), Some(&38));
+        assert_eq!(cheats.get(&((7, 7), (7, 5))), Some(&64));
+
+        assert_eq!(cheats.len(), 44);
+    }
+
+    #[test]
+    fn can_count_significant_cheats() {
+        let track = example_track();
+
+        assert_eq!(track.count_cheats_from(4), 30);
+        assert_eq!(track.count_cheats_from(15), 5);
     }
 }
